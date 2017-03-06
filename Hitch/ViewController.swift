d44ad6d94996ed8e5cc743b423d639e677c3afdb
Sam Goldstein <sammy_goldstein@aol.com>
2017-03-06 01:40:31 -0500

    Second controller up

--- Hitch/ViewController.swift
+++ Hitch/ViewController.swift
@@ -9,7 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //variables for account input
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    @IBAction func signIn(_ sender: Any) {
+        //when Sign In pressed
+        performSegue(withIdentifier: "toSignIn", sender: self)
+    }
+    
+    @IBAction func newAccount(_ sender: Any) {
+        //when New Account pressed
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
